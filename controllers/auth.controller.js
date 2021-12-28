e8d3fface1dd30e6ab0687acb971a0b637e71221
Javier <javicallejon88@gmail.com>
2021-12-28 17:43:04 +0100

    Google Sign In added

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,8 +1,10 @@
-const { response } = require("express");
+const { response, json } = require("express");
 const bcryptjs = require("bcryptjs");
 
 const Usuario = require("../models/users");
 const { generarJWT } = require("../helpers/generar-jwt");
+const { googleVerify } = require("../helpers/google-verify");
+const { DefaultTransporter } = require("google-auth-library");
 
 const login = async (req, res = response) => {
   const { correo, password } = req.body;
@@ -46,6 +48,52 @@ const login = async (req, res = response) => {
   }
 };
 
+const googleSignIn = async (req, res) => {
+  const { id_token } = req.body;
+
+  try {
+    const { nombre, img, correo } = await googleVerify(id_token);
+
+    let usuario = await Usuario.findOne({ correo });
+
+    if (!usuario) {
+      // Si el usuario no existe, lo creamos
+      const data = {
+        nombre,
+        correo,
+        password: "..:P",
+        img,
+        google: true,
+        rol: "USER_ROLE"
+      };
+
+      usuario = new Usuario(data);
+      await usuario.save();
+    }
+
+    // Si el usuario en nuestra bbdd está 'borrado' es decir estado=false
+    if (!usuario.estado) {
+      return res.status(401).json({
+        msg: "Hable con el administrador, usuario bloqueado",
+      });
+    }
+
+    // Generar el JWT
+    const token = await generarJWT(usuario.id);
+
+    res.json({
+      usuario,
+      token
+    });
+  } catch (error) {
+    res.status(400).json({
+      ok: false,
+      msg: "El token no se ha podido verificar.",
+    });
+  }
+};
+
 module.exports = {
   login,
+  googleSignIn,
 };
